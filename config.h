023bb00bdb5925de1b90187b143b76afb9a7e4e4
wur <ilaoser013@gmail.com>
2019-11-02 11:44:26 +0100

    const removed from show_prompt

--- config.h
+++ config.h
@@ -5,5 +5,5 @@
 static const char  ps1_def[]   = "> ";
 static const char  ps1_color[] = "\x1b[32m> \x1b[m";
 static const char *ps1         = ps1_def;
-static const bool  show_prompt = false;
+static       bool  show_prompt = false;
 static const bool  exit_on_err = false;
