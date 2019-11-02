75440434f62183674ed25d1b53eeeb7c20e2e4f2
wur <ilaoser013@gmail.com>
2019-11-02 11:40:51 +0100

    fixed prompt

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define VERSION 0
 #define BUILD   2
 #define HELP_STR \
-"calc -[pPVh]
+"usage: calc -[pPVh]
   -p show prompt
   -P show color prompt
   -V print version and quit 
@@ -32,12 +32,11 @@ main(int argc, char *argv[])
 	/* --- */
 	for (i = 1 ;argv[i] != NULL; i++) {
 		if (argv[i][0] == '-') switch (argv[i][1]) {
-		case 'p': ps1 = ps1_def;    break;
-		case 'P': ps1 = ps1_color;  break;
+		case 'p': ps1 = ps1_def;    show_prompt = true;  break;
+		case 'P': ps1 = ps1_color;  show_prompt = true;  break;
 		case 'V': printf("calc version %i.%i\n", VERSION, BUILD); exit(0);
-		case 'h': puts(HELP_STR); exit(0);
 		default:
-			break;
+		case 'h': puts(HELP_STR); exit(0);
 		};
 	};
 	do {
